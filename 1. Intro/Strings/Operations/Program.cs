d3348fe073aad04ec4da43a54348a2af84d18918
porter-m477 <porter.m477@gmail.com>
2024-04-24 21:03:45 -0600

    Tarea 2

--- 1. Intro/Strings/Operations/Program.cs
+++ 1. Intro/Strings/Operations/Program.cs
@@ -14,34 +14,78 @@ namespace Operations
             string[] strs = { "one", "two", "three", "four" };
 
             // TODO: Propiedad Length 
+            // Retorna la longitud de la cadena de caracteres
+
+            //Console.WriteLine($"The length of str1 is: {str1.Length}");
 
 
             // TODO: Acceder a characters individuales
+            // Accede a un caracter en una posicion especifica
+
+            //Console.WriteLine($"caracter posicion 14 {str1[14]}");
 
 
 
             // TODO: recorre un string como si fuese una secuencia
-
-
-            // TODO: Concatenacion de un string    
-
-
-            // TODO: Uniendo strings con Join
+            // Itera sobre cada caracter de la cadena de caracteres
+            //foreach (char c in str1)
+            //{
+            //    Console.WriteLine(c);
+            //    if (c == 'o')
+            //    {
+            //        Console.WriteLine("Letra o encontrada");
+            //        break;
+            //    }
+            //}
 
 
 
             // TODO: Comparacion entre strings
             // Comparacion entre orden de los caracteres
 
+            //int result = string.Compare(str2, str1);
+            //Console.WriteLine($"Comparacion entre str2 y str3: {result}");
+
+            //if (result == 0)
+            //{
+            //    Console.WriteLine($"Las cadenas son iguales: {str2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Las cadenas son diferentes: {str1},{str2}");
+            //}
+
 
             // TODO: Equals retorna un Boolean
+            // Retorna un valor booleano que indica si dos cadenas son iguales
 
+            //bool areEqual = str2.Equals(str1);
+            //Console.WriteLine($"Return de metodo Equals: {areEqual}");
+
+            //if (areEqual)
+            //{
+            //    Console.WriteLine($"Las cadenas son iguales: {str2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Las cadenas son diferentes: {str1},{str2}");
+            //}
 
 
             // TODO: String index, search y replace
             // https://learn.microsoft.com/en-us/dotnet/api/system.string?view=net-7.0
 
+            Console.WriteLine($"Index of 'fox' in str1: {str1.IndexOf("fox")}");
+            Console.WriteLine($"LastIndex of 'e' in str1: {str1.LastIndexOf("e")}");
 
+            outstr = str1.Replace("quick", "slow");
+            Console.WriteLine($"Replace 'quick' with 'slow': {outstr}");
+
+            string value = "This is a short string.";
+            char delimiter = 's';
+            string[] substrings = value.Split(delimiter);
+            foreach (var substring in substrings)
+                Console.WriteLine(substring);
         }
     }
 }
