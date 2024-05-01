4e0759e3b9eb32bf886a680db5bdeb29bd84c860
Rake91github <raquel.guzman@galileo.edu>
2024-05-01 00:23:53 -0600

    Tarea2

---
+++ Tarea2/Raquel Guzman 23005053/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace TablaMultiplicarWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, numero, result;
+
+            Console.WriteLine("\n\tTarea2");
+            Console.WriteLine("Porfavor ingrese un numero del 1 al 10: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.Write("\nTABLA DE MULTIPLICAR DEL {0} \n\n", numero);
+            while (i <= 10)
+            {
+
+                result = i * numero;
+                Console.WriteLine(i + " X " + numero + " = " + result);
+                i++;
+            }
+            Console.Write("\n\tQue tenga buen dia, Presione <<ENTER>> para finalizar . . . :')");
+            Console.ReadKey();
+        }
+    }
+}
+
