a0211992eef67dd6131a7c8654c52bb70a1e8316
stefanyGRS05 <167147021+stefanyGRS05@users.noreply.github.com>
2024-05-08 21:08:05 -0600

    stefany tarea 2

---
+++ Tarea2/tarea2Stefany.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{ 
+
+    private static void Main(string[] args)
+    {
+
+        int num = 0;
+        
+        while (num<1 || num >10)
+        {
+            Console.WriteLine("Ingrese un numero entero entre el 1 y el 10");
+            num = int.Parse(Console.ReadLine());
+        }
+       
+        static void multiplicar(int num)
+        {
+            int rr;
+            for (int i = 0; i <= 10; i++)
+            {
+                rr = i * num;
+                Console.WriteLine(num + " x " + i + " = " + rr);
+            }
+
+        }
+
+        multiplicar(num);
+
+    }
+}
