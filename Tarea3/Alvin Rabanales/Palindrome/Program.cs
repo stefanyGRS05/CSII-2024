95702209a57fc79dbb5cff865e8956ce5edfda0e
Alvin Rabanales <alvin.rabanales@galileo.edu>
2024-05-10 00:24:10 -0600

    subiendo tarea 3

---
+++ Tarea3/Alvin Rabanales/Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa una frase o palabra: ");
+        string userInput = Console.ReadLine();
+        userInput = new string(userInput.Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).ToArray());
+        Console.WriteLine(EsPalindromo(userInput));
+    }
+
+    public static bool EsPalindromo(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+       
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            sb.Append(input[i]);
+        }
+        
+        string revertido = sb.ToString();
+        
+        if (input == revertido)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
